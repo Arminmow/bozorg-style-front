a5e123762cb5ec3f2744bb1805f4d9d72e4e02bf
Armin <arminmow@gmail.com>
2024-12-26 11:13:12 +0330

    Navbar added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,30 @@
-function Navbar() {
-    return (  <>This is navbar</>);
+import React from 'react';
+import './Navbar.css';  // Importing the custom CSS
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-custom">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">BozorgStyle</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#">زنانه</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">مردانه</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">حراج</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
