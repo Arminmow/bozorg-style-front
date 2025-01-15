eca138b0270fcb170c0f03de8b7a6e66d2741380
Armin <arminmow@gmail.com>
2025-01-15 21:36:39 +0330

    Minor changes

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,9 +3,41 @@ import "./Login.css"; // External CSS
 import loginImg from '../../assets/images/login.jpg'
 import loginImg2 from '../../assets/images/login2.png'
 import { Link } from 'react-router-dom';
+import axios from "axios";
 
 
 function LoginPage() {
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    console.log("Logging in with:");
+    await getCsrfToken();
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/api/register', {
+        name: 'Your Name',
+        email: 'test@example.com',
+        password: 'password',
+      });
+
+      console.log(response.data); // Check the response
+      alert('Registration successful');
+    } catch (err) {
+      console.error(err);
+      alert(err.response?.data?.message || 'Registration failed');
+    }
+  };
+
+  const getCsrfToken = async (e) => {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/sanctum/csrf-cookie');
+      console.log(response);
+      
+      return response;
+    } catch (error) {
+      console.error('Failed to fetch CSRF token', error);
+    }
+  };
+
   return (
     <div className="login-page">
       {/* Form Section */}
@@ -19,11 +51,11 @@ function LoginPage() {
           <p>خوش آمدید . لطفا وارد شوید</p>
         </div>
 
-        <form className="login-form">
+        <form onSubmit={handleSubmit} className="login-form">
           {/* Add your form inputs here */}
           <input type="text" placeholder="نام کاربری" />
           <input type="password" placeholder="رمز" />
-          <button type="submit">ورود</button>
+          <button type="submit">ورود/ ثبت نام</button>
         </form>
       </div>
 
