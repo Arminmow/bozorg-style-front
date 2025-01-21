6b38320dfb53f4c2d3011864ec7f0933db5b8803
Armin <arminmow@gmail.com>
2025-01-21 19:38:31 +0330

    dynamic products page info

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import FilterComponent from "../../components/Filter/Filter";
 import Footer from "../../components/Footer/Footer";
@@ -6,8 +7,10 @@ import Navbar from "../../components/Navbar/Navbar";
 import ProductListing from "../../components/ProductsListing/ProductsListing";
 import axiosInstance from "../../api/axios";
 import "./Products.css";
+import { generateProductPageInfo } from "../../modules/GenerateProductPageInfo/generateProductPageInfo";
 
 function ProductsPage() {
+  const location = useLocation();
   const { gender } = useParams();
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -15,15 +18,16 @@ function ProductsPage() {
   const [filters, setFilters] = useState({
     gender: gender,
   });
+  const { title, description } = generateProductPageInfo(location.pathname);
 
   const fetchProducts = async () => {
     try {
       console.log(filters);
-      
+
       const response = await axiosInstance.get("products", {
         params: filters,
       });
-      
+
       setProducts(response.data);
       setLoading(false);
     } catch (err) {
@@ -61,10 +65,8 @@ function ProductsPage() {
       <Navbar />
       {/* Header section for the category */}
       <div className="category-header">
-        <h1 className="category-title">مردانه</h1>
-        <p className="category-description">
-          استایل مردونه خودتو پیدا کن و بدرخش
-        </p>
+        <h1 className="category-title">{title}</h1>
+        <p className="category-description">{description}</p>
       </div>
       <FilterComponent setFilters={setFilters} filters={filters} />
       <ProductListing products={products} />
