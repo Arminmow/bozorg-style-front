a10dbbcf94900a636e29986c9d9bbc0ac21bbd01
Armin <arminmow@gmail.com>
2025-02-01 17:58:10 +0330

    lazy load

--- src/components/ProductsListing/ProductsListing.jsx
+++ src/components/ProductsListing/ProductsListing.jsx
@@ -2,30 +2,43 @@ import React, { useEffect, useState } from "react";
 import "./ProductsListing.css";
 import { Link } from "react-router-dom";
 
-const ProductListing = ({products}) => {
-  
+const ProductListing = ({ products }) => {
   return (
-    
     <div className="product-listing">
-      { products ? products.map((product) => (
-        <div key={product.id} className="product">
-          <Link to={`/product/${product.id}`} className="product-image-wrapper">
-            <img
-              src={product.images && product.images[0] ? product.images[0].image_path : ''}
-              alt={`Product ${product.id}`}
-              className="product-image default"
-            />
-            <img
-              src={product.images && product.images[1] ? product.images[1].image_path : ''}
-              alt={`Product ${product.id} Hover`}
-              className="product-image hover"
-            />
-          </Link>
-          <div>{product.name}</div>
-          <div className="product-description">{product.description}</div>
-          <div className="product-price">{product.price}</div>
-        </div>
-      )) : ''}
+      {products
+        ? products.map((product) => (
+            <div key={product.id} className="product">
+              <Link
+                to={`/product/${product.id}`}
+                className="product-image-wrapper"
+              >
+                <img
+                  src={
+                    product.images && product.images[0]
+                      ? product.images[0].image_path
+                      : ""
+                  }
+                  alt={`Product ${product.id}`}
+                  className="product-image default"
+                  loading="lazy"
+                />
+                <img
+                  src={
+                    product.images && product.images[1]
+                      ? product.images[1].image_path
+                      : ""
+                  }
+                  alt={`Product ${product.id} Hover`}
+                  className="product-image hover"
+                  loading="lazy"
+                />
+              </Link>
+              <div>{product.name}</div>
+              <div className="product-description">{product.description}</div>
+              <div className="product-price">{product.price}</div>
+            </div>
+          ))
+        : ""}
     </div>
   );
 };
